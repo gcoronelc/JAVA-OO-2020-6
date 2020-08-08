d2b76ed46639ac097a25748159ad5c5bffc6f6c4
Gustavo Coronel <gcoronelc@gmail.com>
2020-08-08 13:34:58 -0500

    Repositorio

---
+++ Clase01/AppPedido/src/pe/edu/uni/apppedido/AppPedido.java
@@ -0,0 +1,21 @@
+package pe.edu.uni.apppedido;
+
+import pe.edu.uni.apppedido.view.PedidoView;
+
+/**
+ * @author Eric Gustavo Coronel Castillo
+ * @blog www.desarrollasoftware.com
+ * @email gcoronelc@gmail.com
+ * @youtube www.youtube.com/c/DesarrollaSoftware
+ * @facebook www.facebook.com/groups/desarrollasoftware/
+ */
+public class AppPedido {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        PedidoView.main(args);
+    }
+
+}
